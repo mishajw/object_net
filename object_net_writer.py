42cc4f54fe7db01f4ac45c5cc7b6c11a4c707785
Misha Wagner <mishajw@gmail.com>
2017-06-27 18:07:16 +0100

    Implement training loop for ObjectNetWriter

--- object_net_writer.py
+++ object_net_writer.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 import numpy as np
-from typing import Callable, List
+from typing import Callable
 
 
 class ObjectNetWriter:
@@ -8,28 +8,65 @@ class ObjectNetWriter:
     GetNextStateFn = Callable[[np.array, float], np.array]
 
     def __init__(self, object_tensor: tf.Tensor, hidden_vector_size: int, get_next_state_fn: GetNextStateFn):
-        hidden_vector = tf.zeros([hidden_vector_size], name="hidden_vector")
+        self.hidden_vector_size = hidden_vector_size
+        self.weights = [
+            tf.Variable(tf.random_normal([self.hidden_vector_size, self.hidden_vector_size + 1])) for _ in range(4)]
+        self.biases = [
+            tf.Variable(tf.random_normal([self.hidden_vector_size + 1])) for _ in range(4)]
+        self.get_next_state_fn = ObjectNetWriter.__wrap_state_function(get_next_state_fn)
 
-        get_next_state_fn = ObjectNetWriter.wrap_state_function(get_next_state_fn)
+        hidden_vector_temp = tf.zeros([self.hidden_vector_size], name="hidden_vector")
+        generated_outputs_temp = tf.zeros([0], name="generated_outputs")
 
-        states = get_next_state_fn([], 0)
+        _, _, self.generated_outputs = tf.while_loop(
+            cond=ObjectNetWriter.__while_condition,
+            body=self.__while_loop,
+            loop_vars=[hidden_vector_temp, object_tensor, generated_outputs_temp],
+            shape_invariants=[
+                tf.TensorShape([self.hidden_vector_size]),  # `hidden_vector` has fixed length
+                tf.TensorShape([None, 2]),  # `object_tensor` will (for now) have be a variable length list of pairs
+                                            # of state and output
+                tf.TensorShape([None])])  # `object_outputs` has variable length
 
-        self.output_vector, self.output_states = tf.while_loop(
-            cond=ObjectNetWriter.check_states_for_end,
-            body=ObjectNetWriter.loop,
-            loop_vars=[hidden_vector, states],
-            shape_invariants=[tf.TensorShape([hidden_vector_size]), tf.TensorShape([None])])
+    def __while_loop(self, hidden_vector, states_and_outputs, object_outputs):
+        current_states_and_outputs = states_and_outputs[0]
+        current_state = current_states_and_outputs[0]
+        current_output = current_states_and_outputs[1]
+
+        weights = tf.case(
+            pred_fn_pairs=[
+                (tf.equal(current_state, 0), lambda: self.weights[0]),
+                (tf.equal(current_state, 1), lambda: self.weights[1]),
+                (tf.equal(current_state, 2), lambda: self.weights[2]),
+                (tf.equal(current_state, 3), lambda: self.weights[3])],
+            default=lambda: self.weights[0],
+            exclusive=True)
+
+        biases = tf.case(
+            pred_fn_pairs=[
+                (tf.equal(current_state, 0), lambda: self.biases[0]),
+                (tf.equal(current_state, 1), lambda: self.biases[1]),
+                (tf.equal(current_state, 2), lambda: self.biases[2]),
+                (tf.equal(current_state, 3), lambda: self.biases[3])],
+            default=lambda: self.biases[0],
+            exclusive=True)
+
+        weights = tf.reshape(weights, [self.hidden_vector_size, self.hidden_vector_size + 1])
+        biases = tf.reshape(biases, [self.hidden_vector_size + 1])
+
+        activations = tf.squeeze(tf.sigmoid(tf.matmul(tf.expand_dims(hidden_vector, axis=0), weights) + biases))
+
+        next_hidden_vector = tf.slice(activations, [0], [self.hidden_vector_size])
+        current_choice = tf.slice(activations, [self.hidden_vector_size], [-1])
+
+        return next_hidden_vector, states_and_outputs[1:], tf.concat([object_outputs, current_choice], axis=0)
 
     @staticmethod
-    def loop(hidden_vector, states):
-        return hidden_vector, states
+    def __while_condition(_, states_and_outputs, __):
+        return tf.not_equal(tf.shape(states_and_outputs)[0], 0)
 
     @staticmethod
-    def check_states_for_end(_, states):
-        return tf.not_equal(tf.shape(states)[0], 0)
-
-    @staticmethod
-    def wrap_state_function(get_next_state_fn: GetNextStateFn) -> GetNextStateFn:
+    def __wrap_state_function(get_next_state_fn: GetNextStateFn) -> GetNextStateFn:
         def wrapped(current_state: [int], current_value: [float]) -> [int]:
             return np.array([int(state.value) for state in get_next_state_fn(current_state, current_value)])
 
