d5245148942377bacf8b79b40f002512c6b74fe5
Misha Wagner <mishajw@gmail.com>
2017-06-28 14:31:50 +0100

    Change construction parameters for ObjectNetWriter

--- object_net_writer.py
+++ object_net_writer.py
@@ -7,7 +7,13 @@ class ObjectNetWriter:
 
     GetNextStateFn = Callable[[np.array, float], np.array]
 
-    def __init__(self, truth_states_and_outputs: tf.Tensor, hidden_vector_size: int, get_next_state_fn: GetNextStateFn):
+    def __init__(
+            self,
+            truth_states: tf.Tensor,
+            truth_outputs: tf.Tensor,
+            hidden_vector_size: int,
+            get_next_state_fn: GetNextStateFn):
+
         self.hidden_vector_size = hidden_vector_size
         self.weights = [
             tf.Variable(tf.random_normal([self.hidden_vector_size, self.hidden_vector_size + 1])) for _ in range(4)]
@@ -21,19 +27,16 @@ class ObjectNetWriter:
         _, _, self.generated_outputs = tf.while_loop(
             cond=ObjectNetWriter.__while_condition,
             body=self.__while_loop,
-            loop_vars=[hidden_vector_temp, truth_states_and_outputs, generated_outputs_temp],
+            loop_vars=[hidden_vector_temp, truth_states, generated_outputs_temp],
             shape_invariants=[
                 tf.TensorShape([self.hidden_vector_size]),  # `hidden_vector` has fixed length
-                tf.TensorShape([None, 2]),  # `object_tensor` will (for now) have be a variable length list of pairs
-                                            # of state and output
+                tf.TensorShape([None]),  # `truth_states` has variable length
                 tf.TensorShape([None])])  # `object_outputs` has variable length
 
-        self.cost = self.__get_cost(truth_states_and_outputs, self.generated_outputs)
+        self.cost = self.__get_cost(truth_outputs, self.generated_outputs)
 
-    def __while_loop(self, hidden_vector, states_and_outputs, object_outputs):
-        current_states_and_outputs = states_and_outputs[0]
-        current_state = current_states_and_outputs[0]
-        current_output = current_states_and_outputs[1]
+    def __while_loop(self, hidden_vector, truth_states, object_outputs):
+        current_state = truth_states[0]
 
         weights = tf.case(
             pred_fn_pairs=[
@@ -61,16 +64,14 @@ class ObjectNetWriter:
         next_hidden_vector = tf.slice(activations, [0], [self.hidden_vector_size])
         current_choice = tf.slice(activations, [self.hidden_vector_size], [-1])
 
-        return next_hidden_vector, states_and_outputs[1:], tf.concat([object_outputs, current_choice], axis=0)
+        return next_hidden_vector, truth_states[1:], tf.concat([object_outputs, current_choice], axis=0)
 
     @staticmethod
-    def __while_condition(_, states_and_outputs, __):
-        return tf.not_equal(tf.shape(states_and_outputs)[0], 0)
+    def __while_condition(_, truth_states, __):
+        return tf.not_equal(tf.shape(truth_states)[0], 0)
 
     @staticmethod
-    def __get_cost(truth_states_and_outputs: tf.Tensor, generated_outputs: tf.Tensor):
-        truth_outputs = tf.slice(truth_states_and_outputs, [0, 1], [-1, 1])
-
+    def __get_cost(truth_outputs: tf.Tensor, generated_outputs: tf.Tensor):
         return tf.sqrt(tf.reduce_mean(tf.square(tf.abs(truth_outputs - generated_outputs))))
 
     @staticmethod
