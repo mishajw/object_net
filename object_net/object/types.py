dcd0b0cd3844b02a03839c8f7999dff6e0cb4123
Misha Wagner <mishajw@gmail.com>
2017-08-28 17:27:28 +0100

    Add creating objects from JSON with tests

--- object_net/object/types.py
+++ object_net/object/types.py
@@ -1,10 +1,26 @@
+import json
 from typing import Dict, List, Any
 from typing import Type as TypingType
+
+
+def assert_string_exists(json_object, name: str):
+    assert name in json_object
+    assert isinstance(json_object[name], str)
+
+
+def assert_string_list_exists(json_object: dict, name: str):
+    assert name in json_object
+    assert isinstance(json_object[name], list)
+    assert all(isinstance(option, str) for option in json_object[name])
 
 
 class Type:
     def __init__(self, name: str):
         self.name = name
+
+    @classmethod
+    def from_json(cls, json_object):
+        raise NotImplementedError()
 
     def validate(self, value):
         raise NotImplementedError()
@@ -22,6 +38,10 @@ class PrimitiveType(Type):
         super().__init__(name)
         self.primitive = primitive
 
+    @classmethod
+    def from_json(cls, json_object):
+        raise TypeError("Can't create a new primitive type from json")
+
     def validate(self, value):
         assert isinstance(value, self.primitive)
 
@@ -31,6 +51,13 @@ class EnumType(Type):
         super().__init__(name)
         self.options = options
 
+    @classmethod
+    def from_json(cls, json_object):
+        assert_string_exists(json_object, "name")
+        assert_string_list_exists(json_object, "options")
+
+        return EnumType(json_object["name"], json_object["options"])
+
     def validate(self, value):
         assert any([value == option for option in self.options])
 
@@ -39,6 +66,13 @@ class UnionType(Type):
     def __init__(self, name: str, types: List[Type]):
         super().__init__(name)
         self.types = types
+
+    @classmethod
+    def from_json(cls, json_object):
+        assert_string_exists(json_object, "name")
+        assert_string_list_exists(json_object, "types")
+
+        return UnionType(json_object["name"], [ReferenceType(type_str) for type_str in json_object["types"]])
 
     def validate(self, value):
         assert [_type.validate(value) for _type in self.types]
@@ -57,6 +91,12 @@ class OptionalType(Type):
         super().__init__("optional[%s]" % _type.name)
         self.type = _type
 
+    @classmethod
+    def from_json(cls, json_object):
+        assert_string_exists(json_object, "type")
+
+        return OptionalType(ReferenceType(json_object["type"]))
+
     def validate(self, value):
         if value is not None:
             self.type.validate(value)
@@ -73,6 +113,15 @@ class ObjectType(Type):
     def __init__(self, name: str, fields: Dict[str, Type]):
         super().__init__(name)
         self.fields = fields
+
+    @classmethod
+    def from_json(cls, json_object: dict):
+        assert_string_exists(json_object, "name")
+
+        fields = dict(
+            (field_name, ReferenceType(json_object[field_name])) for field_name in json_object if field_name != "name")
+
+        return ObjectType(json_object["name"], fields)
 
     def validate(self, value):
         assert isinstance(value, dict)
@@ -94,6 +143,10 @@ class ObjectType(Type):
 class ReferenceType(Type):
     def __init__(self, name: str):
         super().__init__(name)
+
+    @classmethod
+    def from_json(cls, json_object):
+        raise TypeError("Can't create a new reference type from json")
 
     def validate(self, value):
         raise TypeError("Can't validate reference type, must resolve all references first")
@@ -126,3 +179,27 @@ def resolve_references(types: List[Type]):
 
     for _type in types:
         _type.resolve_references(type_dict)
+
+
+def create_from_json(json_str: str) -> List[Type]:
+    json_object = json.loads(json_str)
+
+    return create_from_dict(json_object)
+
+
+def create_from_dict(json_object: dict) -> List[Type]:
+    assert "types" in json_object and isinstance(json_object["types"], list)
+
+    for type_object in json_object["types"]:
+        assert "base" in type_object and isinstance(type_object["base"], str)
+        base = type_object["base"]
+        type_object.pop("base")
+
+        if base == "enum":
+            yield EnumType.from_json(type_object)
+        elif base == "union":
+            yield UnionType.from_json(type_object)
+        elif base == "optional":
+            yield OptionalType.from_json(type_object)
+        elif base == "object":
+            yield ObjectType.from_json(type_object)
--- object_net/tests/object/test_types.py
+++ object_net/tests/object/test_types.py
@@ -23,6 +23,123 @@ class TestTypes(unittest.TestCase):
             },
             tree_types[0])
 
+    def test_create_enum(self):
+        created_types = types.create_from_json(
+            """
+            {
+                "types": [
+                    {
+                        "name": "mod_three",
+                        "base": "enum",
+                        "options": ["one", "two", "three"]
+                    }
+                ]
+            }
+            """)
+
+        mod_three = next(created_types)
+
+        assert isinstance(mod_three, types.EnumType)
+        self.assertIsInstance(mod_three, types.EnumType)
+        self.assertEqual(mod_three.name, "mod_three")
+        self.assertCountEqual(mod_three.options, ["one", "two", "three"])
+
+    def test_create_union(self):
+        created_types = types.create_from_json(
+            """
+            {
+                "types": [
+                    {
+                        "name": "int_or_float",
+                        "base": "union",
+                        "types": ["int", "float"]
+                    }
+                ]
+            }
+            """)
+
+        int_or_float = next(created_types)
+
+        assert isinstance(int_or_float, types.UnionType)
+        self.assertIsInstance(int_or_float, types.UnionType)
+        self.assertEqual(int_or_float.name, "int_or_float")
+        self.assertCountEqual([_type.name for _type in int_or_float.types], ["int", "float"])
+
+    def test_create_optional(self):
+        created_types = types.create_from_json(
+            """
+            {
+                "types": [
+                    {
+                        "base": "optional",
+                        "type": "int"
+                    }
+                ]
+            }
+            """)
+
+        int_opt = next(created_types)
+
+        assert isinstance(int_opt, types.OptionalType)
+        self.assertIsInstance(int_opt, types.OptionalType)
+        self.assertEqual(int_opt.name, "optional[int]")
+        self.assertCountEqual(int_opt.type.name, "int")
+
+    def test_create_object(self):
+        created_types = types.create_from_json(
+            """
+            {
+                "types": [
+                    {
+                        "name": "coordinates",
+                        "base": "object",
+                        "x": "int",
+                        "y": "int"
+                    }
+                ]
+            }
+            """)
+
+        coordinates = next(created_types)
+
+        assert isinstance(coordinates, types.ObjectType)
+        self.assertIsInstance(coordinates, types.ObjectType)
+        self.assertEqual(coordinates.name, "coordinates")
+
+        field_names = coordinates.fields.keys()
+        field_types = [coordinates.fields[field_name].name for field_name in field_names]
+
+        self.assertCountEqual(field_names, ["x", "y"])
+        self.assertCountEqual(field_types, ["int", "int"])
+
+    def test_resolve_references(self):
+        created_types = list(types.create_from_json(
+            """
+            {
+                "types": [
+                    {
+                        "base": "object",
+                        "name": "tree",
+                        "value": "int",
+                        "mod_three": "mod_three",
+                        "left": "optional[tree]",
+                        "right": "optional[tree]"
+                    },
+                    {
+                        "base": "enum",
+                        "name": "mod_three",
+                        "options": ["one", "two", "three"]
+                    },
+                    {
+                        "base": "optional",
+                        "type": "tree"
+                    }
+                ]
+            }
+            """))
+
+        types.resolve_references(created_types)
+
     @staticmethod
     def get_tree_types():
         mod_three = types.EnumType("mod_three", ["one", "two", "three"])
