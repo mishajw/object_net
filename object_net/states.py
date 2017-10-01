23136fc39e064ac60322537f8d01d2b4adafbd65
Misha Wagner <mishajw@gmail.com>
2017-10-01 12:46:37 +0100

    Remove unused code

--- object_net/states.py
+++ object_net/states.py
@@ -40,32 +40,3 @@ class State:
 
     def __str__(self):
         return "State(%s, %d)" % (self.name, self.id)
-
-
-class StateEncoder:
-    """Encode states strings into integers for NumPy arrays"""
-
-    end_state_string = "end"
-
-    def __init__(self, state_strings: [str]):
-        self.state_strings = state_strings
-
-    def encode(self, state_string: str) -> int:
-        assert state_string in self.state_strings
-
-        if state_string == StateEncoder.end_state_string:
-            return 0
-
-        # Plus one because 0 is taken up by end state
-        return self.state_strings.index(state_string) + 1
-
-    def get_end_state(self):
-        return self.encode(StateEncoder.end_state_string)
-
-    def decode(self, state_int: int):
-        assert state_int < len(self.state_strings) + 1
-
-        if state_int == 0:
-            return StateEncoder.end_state_string
-
-        return self.state_strings[state_int + 1]
